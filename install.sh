cf17a55b8312e61ca542c0444a0d7f740ef9a36c
cat <cat@cats-MacBook-Pro.local>
2024-10-02 11:57:24 -0700

    ok

--- install.sh
+++ install.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 #================================================================================ 
+
 # Last Upate: Fri Oct  7 12:39:43 PDT 2016 
 # Tue Nov 15 00:08:03 PST 2016  - add rsync, home dotfile to GoogleDrive/homedotfile
 # 
 # Script to manage all the small tasks such as editing and backup
 #================================================================================ 
 # all the colors in color.sh file
+
 # [gf] open it 
 # how to use in /Users/cat/myfile/script/jav.sh 
 # e.g. printf "${FG_BR_RED}Hello World${RESET_ALL}\n"
 # -------------------------------------------------------------------------------- 
 # Tue May  7 16:46:25 2019 
 # Add full path to ghc, Emacs can't find ghc from M-:
+
 #================================================================================ 
 # Sun Jul 28 18:21:09 2019 
 # update to more generic file path
 #================================================================================ 
 # Tue 12 May 15:07:59 2020 
 # Allow to add new symbol link name under $sym 
+
 #================================================================================ 
 
 # $(basename file.cpp) => file
@@ -26,23 +30,27 @@
 #else
 #fi
 
+
 #if [ "$?" -eq 0 ]; then
 #else
 #fi
 
 #for var in $(ls) 
+
 #do
 #    $echo $var
 #done 
 
 # Mon Aug  5 14:27:58 2019 
 # remove the code to read config.txt => osconfig.txt file
+
 # change to read $b/publicfile/osconfig.txt
 
 # copy binary into $HOME/myfile/mybin/xxxBin/xxx
 # create symbol link in $sym/xxx ->  $HOME/myfile/mybin/xxxBin/xxx
 
 function help(){
+
     printc 196 "help message"
 }
 
@@ -51,10 +59,12 @@ source $HOME/myfile/bitbucket/script/AronLib.sh
 getpwd
 
 
+
 # getName -> $ff/mybin/getName  is Haskell code
 mybin=$HOME/myfile/mybin
 fname=$(getName $PWD)
 dir=${fname}Bin
+
 bindir=$mybin/$dir
 
 printc 200 "[fname=$fname]"
@@ -102,23 +112,19 @@ elif [[ "$#" -eq 1 ]]; then
         cp ./config.txt $bindir 
         ls -lah $bindir
 
-
-	execList=$(cat "${fname}.cabal" | grep '^executable' | awk '{print $2}')
-
+	      execList=$(cat "${fname}.cabal" | grep '^executable' | awk '{print $2}')
 	
         cd $sym
         rm $sym/$fname
 
         # ln -s $bindir/$fname $fname
-
-	for fn in $execList; do
-	    ln -s $bindir/$fn $fn
-	done
-	
+        for fn in $execList; do
+            ln -s $bindir/$fn $fn
+        done
 
         # hard code it since ghci-daemon has two exec files
         # TODO: find a better way to do it
-        # ln -s $bindir/ghci-client ghci-client
+        # ln -s $bindir/ghci-daemon ghci-client
 
         ls -lah $mybin
         ls -lah $sym | grep $fname
